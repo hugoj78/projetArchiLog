b17b4ce10214a1ad7e7412f714820c489d6f8933
hugoj78 <hugo@macbookprohj.home>
2021-01-22 11:22:41 +0100

    add research

--- app/models/manga.rb
+++ app/models/manga.rb
@@ -1,5 +1,9 @@
 class Manga < ApplicationRecord
 	validates :name, presence: true
 	belongs_to :user
-  	has_many :tomes, dependent: :destroy
+	  has_many :tomes, dependent: :destroy
+	  
+	def self.search(search)
+    	where("name LIKE ?", "%#{search}%")
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,6 @@ Rails.application.routes.draw do
   		end
 
 	resource :reviews
+
+	resource :researchs
 end
