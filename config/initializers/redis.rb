6301d959682a10fa96efd99a6e7566aaaab6e8e9
hugoj78 <hugo@macbookprohj.home>
2021-01-28 12:31:00 +0100

    add redis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -3,10 +3,10 @@ $redis = Redis.new
 url = ENV["REDISCLOUD_URL"]
 if url
     Sidekiq.configure_server do |config|
-        config.redis = { url: url ​}
+        config.redis = {url: url}
     end
     Sidekiq.configure_client do |config|
-        config.redis = { url: url}
+        config.redis = {url: url}
     end
     $redis = Redis.new(:url => url)
 end
